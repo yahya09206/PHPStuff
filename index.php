713bc0149c5942e44a879dd87330b8b41064ca01
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-09 22:19:13 -0800

    updated with comments

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
 $greeting = "Hello Universe";
+$name = "Yahya Hussein";
 
+echo "Hello, $name";
 echo $greeting;
+
+//variables start with $
+//can't use single quotes to concatenate strings and variables together
+//how to concatenate with single quotes 'String' . $variable;
