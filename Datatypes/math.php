187a0895e50576fbefd9ccd4b8a16836890bf77f
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-19 10:04:49 -0800

    new lecture on math

--- Datatypes/math.php
+++ Datatypes/math.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="utf-8">
 	<title>Document</title>
 </head>
 <body>
+<?php 
 
+
+
+ ?>
 </body>
 </html>
---
+++ Datatypes/variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php 
+$name = "Yahya</br>";
+$number = 100;
+echo $name;
+echo $name . " " . $number;
+
+
+/*
+variables start with '$'
+concatenate with '.'
+can use html tags inside of strings
+*/
+ ?>
+
+
+
+</body>
+</html>
