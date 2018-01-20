f4b741fe54748c8c08b72e0d951abf9621cf4368
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-20 15:22:07 -0800

    working on array functions

---
+++ Datatypes/arrays.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+</head>
+<body>
+
+
+
+<?php 
+
+$numberList = array();
+
+//how to declare array function 'array()'
+//can store any type of data over and over again
+ ?>
+</body>
+</html>
