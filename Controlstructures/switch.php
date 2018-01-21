0d8e4c4776c5445c238862067f05f07c0349ab20
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-20 22:18:40 -0800

    working on switch statements

--- Controlstructures/switch.php
+++ Controlstructures/switch.php
@@ -6,6 +6,24 @@
 <body>
 <?php 
 
+$number = 4;
+
+//switch statement skeleton
+switch ($number) {
+	case 34:
+		echo "is it 34?";
+		break;
+	case 4:
+		echo "is it 4?";
+		break;
+	case 10:
+		echo "is it 10?";
+		break;
+	
+	default:
+		echo "number doesn't exist";
+		break;
+}
 
  ?>
 </body>
