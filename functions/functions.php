7faa02faab77c21e406649e611633e02aae60351
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-21 00:04:29 -0800

    finished with functions

--- functions/functions.php
+++ functions/functions.php
@@ -7,8 +7,10 @@
 <?php 
 
 function saySomething(){
-	
+	echo "Hey student, do you like the class? yes? great";
 }
+
+saySomething();
 
 
 /*
@@ -18,7 +20,7 @@ function(parameter){
 }
 call function
 functionName(value);
-
+don't need to use 'echo' to print out function
 */
 ?>
 </body>
---
+++ functions/parameters.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>parameters</title>
+</head>
+<body>
+<?php 
+
+
+?>
+</body>
+</html>
