8d2e7a9817e059461f37671935b99720d119d099
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-20 22:13:31 -0800

    starting on switch statements

---
+++ Controlstructures/ifstatements.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>If Statement</title>
+</head>
+<body>
+<?php 
+
+
+
+
+
+// if (3 > 10) {
+// 	# code...
+// 	echo "string";
+// }else{
+// 	echo "broke";
+// }
+
+ ?>
+</body>
+</html>
---
+++ Controlstructures/switch.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Switch</title>
+</head>
+<body>
+<?php 
+
+
+ ?>
+</body>
+</html>
