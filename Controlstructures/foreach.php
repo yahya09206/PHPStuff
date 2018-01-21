7c939389ce7470492db5b59abfaa34c4bc84cc50
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-20 22:43:18 -0800

    finished with foreach

--- Controlstructures/foreach.php
+++ Controlstructures/foreach.php
@@ -4,8 +4,13 @@
 	<title>Foreach</title>
 </head>
 <body>
-<?php 
 
+<?php 
+$numbers = array(34,567,43,223,556,76,432,5);
+	foreach ($numbers as $number) {
+		echo $number . "<br>";
+	}
+//used for looping through arrays without knowing amount of elements in each array	
  ?>
 </body>
 </html>
