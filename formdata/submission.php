b5f2231b8afd844647bfde4e25c8ca91d2f1f45c
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-28 13:11:54 -0800

    setting up form

---
+++ formdata/submission.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form action="">
+		<input type="text" name="">
+		<input type="password" name="">
+		<input type="submit" name="">
+	</form>
+<?php 
+
+
+
+//POST is a global variable that picks up data from form
+?>
+</body>
+</html>
