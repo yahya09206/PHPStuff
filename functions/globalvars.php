ea8d863b63b711668ebd116c2bc6426882957981
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-27 22:15:47 -0800

    finished with global and local scope

--- functions/globalvars.php
+++ functions/globalvars.php
@@ -1,4 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+</head>
+<body>
 <?php 
+$x = "outside"; //global
 
+function convert(){
+	global $x;
+	$x = "inside"; //local
+}
 
+echo $x;
+echo "<br>";
+convert();
+echo $x;
+
+//local scope and global scope
+//global vars are available outside and inside a function
+//local vars can't be used outside a function
+//use keyword 'global' to make variable inside of function a global one
 ?>
+</body>
+</html>
