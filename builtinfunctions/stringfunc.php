447dfc0609033aabce9d6f40c51832c38cd59b70
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-27 22:47:14 -0800

    array functions

--- builtinfunctions/stringfunc.php
+++ builtinfunctions/stringfunc.php
@@ -5,7 +5,12 @@
 </head>
 <body>
 <?php 
-echo "string";
+$string = "Hello student do you like the class?";
+
+echo strlen($string) ;//length of a string
+
+echo "<br>";
+echo strtoupper($string);//convert to upper case
 
 ?>
 </body>
