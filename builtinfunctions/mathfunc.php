ff1e5e09fa6b549de2ac6a34b8b780f448d7de3a
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-27 22:38:11 -0800

    built in math functions

---
+++ builtinfunctions/mathfunc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Math Functions</title>
+</head>
+<body>
+<?php 
+
+echo pow(2,7);//power of a number
+
+echo "<br>";
+
+echo rand(1,1000); //random number
+
+echo "<br>";
+
+echo sqrt(100); //square root of number
+
+echo "<br>";
+
+echo ceil(4.6); //round up
+
+echo "<br>";
+
+echo floor(5.3); //round down
+?>
+</body>
+</html>
