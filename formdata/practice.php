2c9c04c47d2d2834dd08dc31eef8e50ea6b7152e
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-28 14:16:39 -0800

    doing practice problem

---
+++ formdata/practice.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Practice</title>
+</head>
+<body>
+<form action="practice.php" method="post">
+	<input type="text" name="username">
+	<input type="submit" name="submit">
+</form>
+</body>
+</html>
