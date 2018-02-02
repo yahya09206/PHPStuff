d34d445b8eed9d0302b232213ece0e8589b6af9b
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-01 23:39:57 -0800

    creating form

--- loginapp/login.php
+++ loginapp/login.php
@@ -5,6 +5,16 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-
+<div class="container">
+	<div class="col-xs-6">
+		<form action="">
+			<div class="form-group">
+				<input class="form-control">
+					
+				</input>
+			</div>
+		</form>
+	</div>
+</div>
 </body>
 </html>
