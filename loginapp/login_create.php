e558f7268e474c32cfd0ba491871fbb162bd4ae3
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-08 18:32:08 -0800

    created new file for creating queries

---
+++ loginapp/login_create.php
@@ -0,0 +1,52 @@
+<?php 
+if (isset($_POST['submit'])) {
+	# code...
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	//check if data exists
+	// if($username && $password){
+	// 	echo $username;
+	// 	echo $password;
+		
+	// }else{
+	// 	echo "noooo";
+	// }
+
+//connect to database
+$connection = mysqli_connect('localhost', 'root', 'root', 'loginapp', 8889);
+	if($connection){
+		echo "we are connected";
+	}else{
+		//error code
+		die('Database connection failed');
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MySQL</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+	<div class="col-sm-6">
+		<form action="login_create.php" method="post">
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" name="username" class="form-control">
+					
+				</input>
+				<label for="password">Password</label>
+				<input type="password" name="password" class="form-control">
+					
+				</input>
+			</div>
+			<input class="btn btn-primary" type="submit" name="submit" value="Submit">
+		</form>
+	</div>
+</div>
+</body>
+</html>
