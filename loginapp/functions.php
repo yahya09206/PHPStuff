2a2fd5962bda55ed91d8854bf78e3570f83993be
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-08 19:48:31 -0800

    still refactoring

--- loginapp/functions.php
+++ loginapp/functions.php
@@ -1,11 +1,20 @@
 <?php 
-//UPDATE
-	//select everything from users
-	$query = "SELECT * FROM users";
-	//prebuilt function for connecting with db that accepts two parameters
-	$result = mysqli_query($connection, $query);
-	if(!result){
-		die('Query FAILED' . mysqli_error());
-	}
+
+function showAllData(){
+	//UPDATE
+		//select everything from users
+		$query = "SELECT * FROM users";
+		//prebuilt function for connecting with db that accepts two parameters
+		$result = mysqli_query($connection, $query);
+		if(!result){
+			die('Query FAILED' . mysqli_error());
+		}
+
+		//pull data from database from query up above using $result
+		while ($row = mysqli_fetch_assoc($result)) {
+			$id = $row['id'];
+			echo "<option value='id'>$id</option>";
+		}
+}
 
 ?>
