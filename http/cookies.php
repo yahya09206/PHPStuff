16f7d531107edd50f73064b2621aa4e8fc3a0e67
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-17 20:50:39 -0800

    setting up cookie function

---
+++ http/cookies.php
@@ -0,0 +1,16 @@
+<?php 
+	//set cookies takes 3 params
+	$name = 'John';
+	$value = 100;
+	$expire = 
+	setcookie('', '', '');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
