d941d7d4fc211cfeed96404e24b40f97d722aa52
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-19 14:03:08 -0800

    setting properties

--- classes/instantiate.php
+++ classes/instantiate.php
@@ -1,13 +1,21 @@
 <?php 
 //class syntax
 class Car{
+	var $wheels = 4;
+	var $hood = 1;
+	var $engine = 1;
+	var $door = 4;
 	function moveWheels(){
-		echo "car is moving";
+		echo "car is moving ";
 	}
 }
 
 //new instance of car
 $audi = new Car();
 $benz = new Car();
+
+//use method on car
+$audi->moveWheels();
+$benz->moveWheels();
 
 ?>
