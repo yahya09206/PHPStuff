3da84fc9cea9615497985b08a0321194dd04581e
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-19 14:15:00 -0800

    finished setting properties

--- classes/instantiate.php
+++ classes/instantiate.php
@@ -5,17 +5,30 @@ class Car{
 	var $hood = 1;
 	var $engine = 1;
 	var $door = 4;
+
 	function moveWheels(){
-		echo "car is moving ";
+		//use keyword this to reference 
+		$this->wheels = 10;
+	}
+
+	function doors(){
+		//use keyword this to reference 
+		$this->doors = 6;
 	}
 }
 
 //new instance of car
 $audi = new Car();
-$benz = new Car();
-
+$truck = new Car();
 //use method on car
+// $audi->moveWheels();
+
+//change value of property
 $audi->moveWheels();
-$benz->moveWheels();
+//assign property
+echo $audi->wheels . "<br>";
+
+echo $truck->wheels = 10;
+echo $truck->doors = 6;
 
 ?>
